ca0663de0a8d25a83b4403b7f772a5cf52f7dada
kanameriko <suzumiyadoll@outlook.com>
2019-04-09 14:05:59 +0800

    lombok添加

--- springboot-shop/src/main/java/com/sarnath/sshop/entity/UserEntity.java
+++ springboot-shop/src/main/java/com/sarnath/sshop/entity/UserEntity.java
@@ -8,6 +8,7 @@ import java.util.Date;
 @Getter
 @Setter
 public class UserEntity {
+
     private Integer userId;
     private String userName;
     private String userEmail;
--- springboot-shop/src/main/java/com/sarnath/sshop/entity/form/UserForm.java
+++ springboot-shop/src/main/java/com/sarnath/sshop/entity/form/UserForm.java
@@ -1,9 +1,14 @@
 package com.sarnath.sshop.entity.form;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+@Getter
+@Setter
 public class UserForm {
     @NotEmpty(message = "userName不能为空")
     private String userName;
@@ -15,53 +20,4 @@ public class UserForm {
     private String userImg;
     @NotEmpty(message = "userPassword不能为空")
     private String userPassword;
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserEmail() {
-        return userEmail;
-    }
-
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
-    }
-
-    public Integer getUserSex() {
-        return userSex;
-    }
-
-    public void setUserSex(Integer userSex) {
-        this.userSex = userSex;
-    }
-
-    public Integer getUserPhone() {
-        return userPhone;
-    }
-
-    public void setUserPhone(Integer userPhone) {
-        this.userPhone = userPhone;
-    }
-
-    public String getUserImg() {
-        return userImg;
-    }
-
-    public void setUserImg(String userImg) {
-        this.userImg = userImg;
-    }
-
-    public String getUserPassword() {
-        return userPassword;
-    }
-
-    public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword;
-    }
-
 }
